ad342e583e9732ad44b13dd793bd089e95c82571
Italo Silva <italo.vct@gmail.com>
2025-03-14 13:52:14 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,21 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1,"Sequissu");
-		System.out.println(obj);
 		
-		Seller sl = new Seller(1,"Maria","maria@gmail.com", new Date(),2000.0,obj);
-		System.out.println(sl);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
+		
+		
 	}
 
 }
