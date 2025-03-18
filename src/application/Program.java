84a8cecf75216283dbd25867ad667e0eacad0c1f
Italo Silva <italo.vct@gmail.com>
2025-03-18 13:27:37 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -26,12 +27,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("=== TESTE 2: seller fingAll ===");
+		System.out.println("=== TESTE 3: seller findAll ===");
 		list = sellerDao.findAll();
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
 		
+		System.out.println("=== TESTE 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 		
 	}
 
